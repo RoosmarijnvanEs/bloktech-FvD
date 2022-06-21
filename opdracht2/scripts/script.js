ac9640a6d7c9d9c3c8e1d36b3e18f0b8409db410
Roosmarijn van Es <103881571+RoosmarijnvanEs@users.noreply.github.com>
2022-06-21 16:06:54 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -8,9 +8,6 @@ var dezeWeek = document.querySelector("main ol li:nth-of-type(2) ul");
 var volgendeWeek = document.querySelector("main ol li:nth-of-type(3) ul");
 var galerijLijst = document.querySelector("section ul");
 
-/**********************/
-/* Carousel */
-/**********************/
 
 /**********************/
 /* DRAGGEN EN DROPPEN */
@@ -50,99 +47,19 @@ new Sortable(volgendeWeek, {
 });
 
 /**********************/
-/* Carousel */
-/*Bron: https://www.youtube.com/watch?v=gBzsE0oieio*/
-/*Bron: https://www.youtube.com/watch?v=VYsVOamdB0g*/
-/**********************/
-
-// const track = document.querySelector('.carousel_track');
-// const slides = Array.from(track.children);
-// const nextButton = document.querySelector('.carousel_button--right');
-// const prevButton = document.querySelector('.carousel_button--left'); 
-// const dotsNav = document.querySelector('.carousel_nav');
-// const dots = Array.from(dotsNav.children);
-
-// const slideWidth = slides[0].getBoundingClientRect().width;
-
-// const setSlidePosition = (slide, index) => {
-// 	slide.style.left = slideWidth * index + 'px';
-// };
-// slides.forEach(setSlidePosition);
-
-// const moveToSlide = (track, currentSlide, targetSlide) =>{
-// 	track.style.transform = 'translateX (-'+ targetSlide.style.left + ')';
-// 	currentSlide.classList.remove('current-slide');
-// 	targetSlide.classList.add('current-slide');
-// }
-
-// const updateDots = (currentDot, targetDot) => {
-// 	currentDot.classList.remove('current-slide');
-// 	targetDot.classList.add('current-slide');
-// }
-
-// const hideShowArrows = () => {
-// 	if (targetIndex === 0) {
-// 		prevButton.classList.add('is-hidden');
-// 		nextButton.classList.remove('is-hidden');
-// 	}else if (targetIndex === slides.length - 1){
-// 		prevButton.classList.remove('is-hidden');
-// 		nextButton.classList.add('is-hidden');
-// 	}else{
-// 		prevButton,classlist.remove('is-hidden');
-// 		nextButton,classlist.remove('is-hidden');
-// 	}
-// }
-
-// prevButton.addEventListener('click', e =>{
-// 	const currentSlide = track.querySelector('.current-slide');
-// 	const prevSlide = currentSlide.previousElementSibling;
-// 	const currentDot = dotsNav.querySelector('.current-slide');
-// 	const prevDot = currentDot.previousElementSibling;
-// 	const prevIndex = slides.findIndex(slide => slide === prevSlide);
-
-// 	moveToSlide(track, currentSlide, prevSlide);
-// 	updateDots(currentDot, prevDot);
-// 	hideShowArrows(slides, prevButton, nextButton, prevIndex);
-// });
-
-// nextButton.addEventListener('click', e =>{
-// 	const currentSlide = track.querySelector('.current-slide');
-// 	const nextSlide = currentSlide.nextElementSibling;
-// 	const currentDot = dotsNav.querySelector('.current-slide');
-// 	const nextDot = currentDot.nextElementSibling;
-// 	const nextIndex = slides.findIndex(slide => slide === nextSlide);
-
-// 	moveToSlide(track, currentSlide, nextSlide);
-// 	updateDots(currentDot, nextDot);
-// 	hideShowArrows(slides, prevButton, nextButton, nextIndex);
-// });
-
-// dotsNav.addEventListener('click', e =>{
-// const targetDot = e.target.closest('button');
-
-// if (!targetDot) return;
-
-// const currentSlide = track.querySelector('.current-slide');
-// const currentDot = dotsNav.querySelector('.current-slide');
-// const targetIndex = dots.findIndex(dot => dot === targetDot);
-// const targetSlide = slides[targetIndex];
-
-// moveToSlide(track, currentSlide, targetSlide);
-// updateDots(currentDot, targetDot);
-// hideShowArrows(slides, prevButton, nextButton, targetIndex);
-
-
-// })
-
-/**********************/
 /* venster */
 /* bron: https://www.youtube.com/watch?v=MBaw_6cPmAw */
 /**********************/
 
+
+// Aanroepen van de button
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
+
+// Aanroepen overlaagje
 const overlay = document.getElementById('overlay')
 
+// Bij elke keer klikken wordt data geactiveert van de aside
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
@@ -150,26 +67,30 @@ openModalButtons.forEach(button => {
   })
 })
 
+// Activeert de overlay
 overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
+  const modals = document.querySelectorAll('aside.active')
   modals.forEach(modal => {
     closeModal(modal)
   })
 })
 
+// Wordt de aside gesloten bij click
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
+    const modal = button.closest('aside')
     closeModal(modal)
   })
 })
 
+// Als aside null is dan actief
 function openModal(modal) {
   if (modal == null) return
   modal.classList.add('active')
   overlay.classList.add('active')
 }
 
+// Als aside niet nul is dan verwijderen
 function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('active')
